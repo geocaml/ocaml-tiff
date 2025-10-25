af7bb52a2238037af4920c6a11db9255b2cba7f1
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2025-10-26 00:15:37 +0100

    Implement DEFLATE decompression using decompress.zl library

--- src/deflate.ml
+++ src/deflate.ml
@@ -1,4 +1,4 @@
-(* DEFLATE decompression for TIFF files using the decompress library *)
+(* DEFLATE/Zlib decompression for TIFF files using the decompress library *)
 
 module Unsafe = struct
   external unsafe_blit_string_to_bigstring :
@@ -14,10 +14,15 @@ let decode input output_buffer =
   let input_len = Cstruct.length input in
   let output_len = Cstruct.length output_buffer in
 
+  if input_len = 0 then
+    failwith "Empty DEFLATE input"
+  else if output_len = 0 then
+    failwith "Empty DEFLATE output buffer";
+
   (* Convert Cstruct to string for decompress library *)
   let input_string = Cstruct.to_string input in
 
-  (* Use the Higher interface for DEFLATE decompression *)
+  (* Use the Higher interface for Zlib decompression *)
   let input_pos = ref 0 in
   let output_pos = ref 0 in
 
@@ -26,11 +31,10 @@ let decode input output_buffer =
     if remaining > 0 then (
       let to_read = min remaining (Bigstringaf.length buf) in
       let input_substring = String.sub input_string !input_pos to_read in
-      Bigstringaf.blit_from_string ~src_off:0 ~dst_off:0 ~len:to_read
-        input_substring buf;
+      Bigstringaf.blit_from_string ~src_off:0 ~dst_off:0 ~len:to_read input_substring buf;
       input_pos := !input_pos + to_read;
-      to_read)
-    else 0
+      to_read
+    ) else 0
   in
 
   let flush buf len =
@@ -38,29 +42,23 @@ let decode input output_buffer =
     if to_write > 0 then (
       let output_string = Bigstringaf.substring buf ~off:0 ~len:to_write in
       Unsafe.blit_string output_string 0 output_buffer !output_pos to_write;
-      output_pos := !output_pos + to_write)
+      output_pos := !output_pos + to_write
+    )
   in
 
-  (* Create a window for decompression *)
-  let window = De.make_window ~bits:15 in
-
-  (* Create input and output bigstrings *)
-  let input_bigstring : De.bigstring =
-    Bigarray.Array1.create Bigarray.char Bigarray.c_layout input_len
-  in
-  let output_bigstring : De.bigstring =
-    Bigarray.Array1.create Bigarray.char Bigarray.c_layout output_len
-  in
-
-  (* Copy input string to bigstring *)
+  (* Use the Higher interface for Zlib *)
+  let allocate bits = De.make_window ~bits in
+  let uncompress_fn = Zl.Higher.uncompress ~allocate ~refill ~flush in
+  
+  (* Create input bigstring *)
+  let input_bigstring = Bigarray.Array1.create Bigarray.char Bigarray.c_layout input_len in
   for i = 0 to input_len - 1 do
     Bigarray.Array1.set input_bigstring i input_string.[i]
   done;
-
-  (* Use the Higher interface *)
-  match
-    De.Higher.uncompress ~refill ~flush ~w:window input_bigstring
-      output_bigstring
-  with
+  
+  (* Create output bigstring *)
+  let output_bigstring = Bigarray.Array1.create Bigarray.char Bigarray.c_layout output_len in
+  
+  match uncompress_fn input_bigstring output_bigstring with
   | Ok () -> ()
   | Error (`Msg err) -> failwith ("DEFLATE decompression error: " ^ err)
