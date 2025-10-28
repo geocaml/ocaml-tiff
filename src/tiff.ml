62c1cf62b1e77af2738e8884d8b46b7f42abe9be
prithika.ki <kiprithika@gmail.com>
2025-10-28 19:46:17 +0530

    Fix failed cases

--- src/tiff.ml
+++ src/tiff.ml
@@ -4,13 +4,6 @@ module File = File
 module Endian = Endian
 module Ifd = Ifd
 
-module File = struct
-  type ro = file_offset:Optint.Int63.t -> Cstruct.t list -> unit
-  (** Read-only access to a file that supports reading at a particular offset.
-      The read should be exact and raise [End_of_file] should that be the case.
-  *)
-end
-
 type header = Ifd.header
 
 type ('repr, 'kind) kind =
