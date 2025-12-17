b70b8947e3e6b960bf31456d730297f495c180ab
giftcup <salometambe@gmail.com>
2025-12-17 09:41:14 +0100

    Run benchmark tests with Eio read

--- bench/bench.ml
+++ bench/bench.ml
@@ -5,22 +5,25 @@ type test_case = E : ('a, 'b) Tiff.kind * string -> test_case
 
 let tests =
   [
+    E (Tiff.Uint8, "../test/data/chief_mountain.tiff");
     E (Tiff.Uint8, "../test/data/uniform_uint8_lzw.tiff");
     E (Tiff.Uint8, "../test/data/striped_uint8_uncompressed.tiff");
     E (Tiff.Uint16, "../test/data/uniform_uint16_lzw.tiff");
     E (Tiff.Float32, "../test/data/uniform_float32_lzw.tiff");
+    E (Tiff.Uint8, "../test/data/english_kitchen.tiff");
+    E (Tiff.Uint8, "../test/data/ammunition.tiff");
   ]
 
-let get_dims (E (kind, file)) =
+let get_dims fs (E (kind, file)) =
   Staged.stage @@ fun () ->
-  Tiff_unix.with_open_in file @@ fun ro ->
+  Tiff_eio.with_open_in fs file @@ fun ro ->
   let tiff = Tiff.from_file kind ro in
   let data = Tiff.data tiff ro in
   Sys.opaque_identity (ignore data)
 
-let get_ifd (E (kind, file)) =
+let get_ifd fs (E (kind, file)) =
   Staged.stage @@ fun () ->
-  Tiff_unix.with_open_in file @@ fun ro ->
+  Tiff_eio.with_open_in fs file @@ fun ro ->
   let tiff = Tiff.from_file kind ro in
   let ifd = Tiff.ifd tiff in
   let compression = Tiff.Ifd.compression ifd in
@@ -47,13 +50,15 @@ let lzw (name, buf, expected_size) =
   let output = Cstruct.create expected_size in
   Tiff.Private.Lzw.decode buf output
 
-let tests fn =
+let tests fn fs =
   List.map
     (fun (E (_, file) as e) ->
-      Test.make ~name:(Filename.basename file |> Filename.chop_extension) (fn e))
+      Test.make
+        ~name:(Filename.basename file |> Filename.chop_extension)
+        (fn fs e))
     tests
 
-let benchmark () =
+let benchmark fs () =
   let ols =
     Analyze.ols ~bootstrap:0 ~r_square:true ~predictors:Measure.[| run |]
   in
@@ -63,8 +68,8 @@ let benchmark () =
   let cfg =
     Benchmark.cfg ~limit:3000 ~quota:(Time.second 2.5) ~kde:(Some 1000) ()
   in
-  let read = Test.make_grouped ~name:"read" (tests get_dims) in
-  let ifd = Test.make_grouped ~name:"ifd" (tests get_ifd) in
+  let read = Test.make_grouped ~name:"read" (tests get_dims fs) in
+  let ifd = Test.make_grouped ~name:"ifd" (tests get_ifd fs) in
   let lzw = Test.make_grouped ~name:"lzw" (List.map lzw lzw_bufs) in
   let test = Test.make_grouped ~name:"tiff" [ lzw; ifd; read ] in
   let raw_results = Benchmark.all cfg instances test in
@@ -86,10 +91,12 @@ let img (window, results) =
 open Notty_unix
 
 let () =
+  Eio_main.run @@ fun env ->
+  let fs = Eio.Stdenv.fs env in
   let window =
     match winsize Unix.stdout with
     | Some (w, h) -> { Bechamel_notty.w; h }
     | None -> { Bechamel_notty.w = 80; h = 1 }
   in
-  let results, _ = benchmark () in
+  let results, _ = benchmark fs () in
   img (window, results) |> eol |> output_image
