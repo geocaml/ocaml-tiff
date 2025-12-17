bd9e308ab81d44152bb783d4cb3312478d4c801c
giftcup <salometambe@gmail.com>
2025-12-17 09:39:11 +0100

    Move the call of the eio main function to the executable using defined functions

--- src/eio/tiff_eio.ml
+++ src/eio/tiff_eio.ml
@@ -1,6 +1,6 @@
-let with_open_in path fn =
-  Eio_main.run @@ fun env ->
-  let fs = Eio.Stdenv.fs env in
+(* Provides utitilities for working with TIFF files using the Eio library *)
+
+let with_open_in fs path fn =
   Eio.Path.(with_open_in (fs / path)) @@ fun r ->
   let ro = Eio.File.pread_exact r in
   fn ro
--- src/eio/tiff_eio.mli
+++ src/eio/tiff_eio.mli
@@ -1,1 +1,4 @@
-val with_open_in : string -> (Tiff.File.ro -> 'a) -> 'a
+val with_open_in : _ Eio.Path.t -> string -> (Tiff.File.ro -> 'a) -> 'a
+(** [with_open_in fs path fn] opens [fs/path] as a read-only file The
+    {! Tiff.File.ro} is only valid for the scope of [fn] after which the
+    underlying file will be closed. *)
